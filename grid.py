24611401d85154a67078e884c2eba7b61b93fecd
javvacake <sophia_elisha@yahoo.co.uk>
2022-08-21 23:16:15 +0100

    Need to work on new line. Hopefully .gitignore file works

--- grid.py
+++ grid.py
@@ -3,46 +3,61 @@ import pygame
 from colours import GREEN, YELLOW, GREY, BLACK, WHITE
 
 pygame.font.init()
-FONT = pygame.font.Font("helveticaneue\Helvetica Neu Bold.ttf", 20)
+FONT = pygame.font.Font("helveticaneue\Helvetica Neu Bold.ttf", 60)
 
 LETTERS = list(string.ascii_uppercase)
 
 class Grid:
     def __init__(self):
-        self.matrix = [[Cell(i, j)] for j in range(5) for i in range(6)]
+        self.matrix = [[Square(i, j) for i in range(5)] for j in range(6)]
         self.current_row = 0
         self.current_col = 0
 
     # TODO: finish function
     def enter_letter(self, letter):
-        letter = chr(letter).upper()
-        if letter in LETTERS:
-            if self.current_col == 5:
-                return
-            else:
-                self.matrix[self.current_row][self.current_col].assign_letter(letter)
-                self.current_col += 1
+        print("To enter: ", self.current_row, " ", self.current_col)
+        letter = letter.upper()
+        if self.current_col == 5:
+            return
+        
+        self.matrix[self.current_row][self.current_col].set_letter(letter)
+        self.current_col += 1
+        # print("Enter_letter row/col: ", self.current_row, " ", self.current_col)
 
     # TODO: finish function
     def backspace(self):
-        if self.current_col < 1:
+        if self.current_col < 0:
+            self.current_col = 0
             return
+        
+        self.current_col = self.current_col - 1
+        self.matrix[self.current_row][self.current_col].set_letter(None)
+
+    def get_word(self):
+        word = ""
+        for square in self.matrix[self.current_row]:
+            try:
+                word += square.get_letter()
+            except:
+                return word
+        return word
 
     def draw(self, window):
+        # print("drawing cells")
         for row in self.matrix:
-            for cell in row:
-                cell.draw(window)
+            for square in row:
+                square.draw(window)
 
-class Cell:
-    def __init__(self, row, col):
+class Square:
+    def __init__(self, col, row):
         self.row = row
         self.col = col
         self.letter = None
         self.bg_colour = None
-        self.x = (col + 1) * 60
-        self.y = (row + 1) * 60
-        self.size = 50
-        self.entered = False
+        self.size = 60
+        self.x = (col + 1) * (self.size + 15)
+        self.y = (row + 1) * (self.size + 15)
+        self.entered_row = False
 
     def get_letter(self):
         return self.letter
@@ -55,6 +70,19 @@ class Cell:
     
     # TODO: finish function
     def draw(self, window):
-        bg_colour = self.bg_colour if self.entered else WHITE
+        bg_colour = self.bg_colour if self.entered_row else WHITE
         
-        pygame.draw.rect(window, bg_colour, pygame.Rect(self.x, self.y, self.size, self.size))
+        # Border rectangle
+        border_colour = BLACK
+        pygame.draw.rect(window, border_colour, pygame.Rect(self.x, self.y, self.size, self.size))
+
+        # if not self.entered_row:
+        # Actual rectangle
+        pygame.draw.rect(window, bg_colour, pygame.Rect(self.x + 2, self.y + 2, self.size - 4, self.size - 4))
+
+        if self.letter != None:
+            # Add text
+            text_col = BLACK # if not self.entered_row else WHITE
+            text = FONT.render(self.letter, False, text_col)
+            window.blit(text, (self.x + 10, self.y - 5))
+            # print("draw(cell) row/col: ", self.row, " ", self.col)
--- ui.py
+++ ui.py
@@ -1,21 +1,41 @@
 import pygame
-from wordle import Wordle
+import random
+from words import *
+# from wordle import Wordle
 from grid import Grid
-from colours import WHITE
+from letters import LETTERS
+from colours import *
 
 pygame.init()
+
 pygame.font.init()
 FONT = pygame.font.Font("helveticaneue\Helvetica Neu Bold.ttf", 20)
 
-class UserInterface:
+class UserInterface():
     WIDTH = 500
     HEIGHT = 700
 
     def __init__(self):
         self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
+        pygame.display.set_caption("Wordle (Fake)")
+        self.final_word = self.generate_word()
         self.grid = Grid()
-        self.guess = 1
+        self.guess = 0
+        self.letter_count = 0
+        print(self.final_word) # Test
         self.event_handler()
+
+    def generate_word(self):
+        return random.choice(WORDS)
+
+    def check_real_word(self, word):
+        return True if word in WORDS else False
+
+    def error_message(self, text):
+        message_box = FONT.render(text, False, BLACK)
+        self.screen.blit(message_box, (90, 620))
+        pygame.display.update()
+        pygame.time.delay(500)
 
     def render(self):
         self.screen.fill(WHITE)
@@ -27,25 +47,68 @@ class UserInterface:
         running = True
 
         while running:
+            if self.letter_count < 0:
+                self.letter_count = 0
+
             for event in pygame.event.get():
                 # Player closes window
                 if event.type == pygame.QUIT:
                     print("Player chose to quit")
                     pygame.quit()
                 if event.type == pygame.KEYDOWN:
+                    print(pygame.key.name(event.key) + " " + str(self.letter_count)) # Test
+
+                    # if self.letter_count > 5:
+                    #         self.error_message("1 Please input a 5-letter word")
+                    #         pass #?
+
                     if event.key == pygame.K_BACKSPACE:
                         self.grid.backspace()
-                    # elif event.key == pygame.K_RETURN:
+                        self.letter_count -= 1
+                        # self.render()
+
+                    elif pygame.key.name(event.key) in LETTERS:
+                        print("In alphabet") # Test
+                        self.letter_count += 1
+                        if self.letter_count > 5: # Account for backspace
+                            self.error_message("Out of space")
+
+                        input_letter = pygame.key.name(event.key)
+                        self.grid.enter_letter(input_letter)
+                        # self.render()
+
+                    elif pygame.key.name(event.key) not in LETTERS and event.key != pygame.K_RETURN:
+                        self.error_message("Please input a letter")
+                        print("Not in alphabet") # Test
+
+                    elif event.key == pygame.K_RETURN:
+                        if self.letter_count < 5:
+                            self.error_message("2 Please input a 5-letter word")
+                            # self.grid.backspace()
+
+                        self.letter_count = 0
+                        guessed_word = self.grid.get_word().lower()
                         # Check if word is correct
-                        # If not...
+                        if self.check_real_word(guessed_word):
+                            # Do the colour thing
+                            print("valid word") # Test
+                            # self.render()
+                        else:
+                            self.error_message("Word not in directory")
+                            print("Word not in directory") # Test
+                            pass
+
+                        if self.final_word == guessed_word:
+                            print("YAY") # Test
+                            # self.render()
+                    # else:
+                    #     input_letter = pygame.key.name(event.key)
+                    #     self.grid.enter_letter(input_letter)
 
             # No more guesses left
-            if self.guess > 6:
+            if self.guess > 5:
                 self.render()
 
             self.render() # TODO: throws traceback error
 
         pygame.quit()
-        
-#     screen.fill((255, 255, 255)) # Fill background with white
-#     pygame.display.flip() # Flip the display? Acc turns it white

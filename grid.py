5c74ff7560b060531267b9c0821620ba557f1253
javvacake <sophia_elisha@yahoo.co.uk>
2022-08-22 01:46:57 +0100

    Forgot to commit these

--- grid.py
+++ grid.py
@@ -1,9 +1,9 @@
 import string
 import pygame
-from colours import GREEN, YELLOW, GREY, BLACK, WHITE
+from colours import *
 
 pygame.font.init()
-FONT = pygame.font.Font("helveticaneue\Helvetica Neu Bold.ttf", 60)
+FONT = pygame.font.Font("assets\Helvetica Neu Bold.ttf", 60)
 
 LETTERS = list(string.ascii_uppercase)
 
@@ -13,9 +13,8 @@ class Grid:
         self.current_row = 0
         self.current_col = 0
 
-    # TODO: finish function
     def enter_letter(self, letter):
-        print("To enter: ", self.current_row, " ", self.current_col)
+        # print("To enter: ", self.current_row, " ", self.current_col)
         letter = letter.upper()
         if self.current_col == 5:
             return
@@ -24,7 +23,6 @@ class Grid:
         self.current_col += 1
         # print("Enter_letter row/col: ", self.current_row, " ", self.current_col)
 
-    # TODO: finish function
     def backspace(self):
         if self.current_col < 0:
             self.current_col = 0
@@ -42,6 +40,14 @@ class Grid:
                 return word
         return word
 
+    def enter_word(self, results):
+        # Recolour each square in row using results
+        for index, square in enumerate(self.matrix[self.current_row]):
+            square.set_bg_colour(results[index])
+
+        self.current_row += 1
+        self.current_col = 0
+
     def draw(self, window):
         # print("drawing cells")
         for row in self.matrix:
@@ -53,8 +59,8 @@ class Square:
         self.row = row
         self.col = col
         self.letter = None
-        self.bg_colour = None
-        self.size = 60
+        self.bg_colour = WHITE
+        self.size = 70
         self.x = (col + 1) * (self.size + 15)
         self.y = (row + 1) * (self.size + 15)
         self.entered_row = False
@@ -68,9 +74,12 @@ class Square:
     def delete_letter(self):
         self.letter = None
     
+    def set_bg_colour(self, colour):
+        self.bg_colour = colour
+    
     # TODO: finish function
     def draw(self, window):
-        bg_colour = self.bg_colour if self.entered_row else WHITE
+        # bg_colour = self.bg_colour if self.entered_row else WHITE
         
         # Border rectangle
         border_colour = BLACK
@@ -78,11 +87,11 @@ class Square:
 
         # if not self.entered_row:
         # Actual rectangle
-        pygame.draw.rect(window, bg_colour, pygame.Rect(self.x + 2, self.y + 2, self.size - 4, self.size - 4))
+        pygame.draw.rect(window, self.bg_colour, pygame.Rect(self.x + 2, self.y + 2, self.size - 4, self.size - 4))
 
         if self.letter != None:
             # Add text
             text_col = BLACK # if not self.entered_row else WHITE
             text = FONT.render(self.letter, False, text_col)
-            window.blit(text, (self.x + 10, self.y - 5))
+            window.blit(text, (self.x + 10, self.y))
             # print("draw(cell) row/col: ", self.row, " ", self.col)
--- ui.py
+++ ui.py
@@ -1,23 +1,25 @@
 import pygame
 import random
 from words import *
-# from wordle import Wordle
 from grid import Grid
+from rules import *
 from letters import LETTERS
 from colours import *
 
 pygame.init()
 
 pygame.font.init()
-FONT = pygame.font.Font("helveticaneue\Helvetica Neu Bold.ttf", 20)
+FONT = pygame.font.Font("assets\Helvetica Neu Bold.ttf", 20)
+ICON = pygame.image.load("assets\icon.png")
 
 class UserInterface():
-    WIDTH = 500
-    HEIGHT = 700
+    WIDTH = 575
+    HEIGHT = 800
 
     def __init__(self):
         self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
         pygame.display.set_caption("Wordle (Fake)")
+        pygame.display.set_icon(ICON)
         self.final_word = self.generate_word()
         self.grid = Grid()
         self.guess = 0
@@ -26,89 +28,94 @@ class UserInterface():
         self.event_handler()
 
     def generate_word(self):
-        return random.choice(WORDS)
+        return "socks" # Test
+        # return random.choice(WORDS)
 
     def check_real_word(self, word):
         return True if word in WORDS else False
 
-    def error_message(self, text):
+    def display_message(self, text, end: bool):
+        if end:
+            text = "The word was {}. Better luck next time!".format(self.final_word)
+
         message_box = FONT.render(text, False, BLACK)
         self.screen.blit(message_box, (90, 620))
         pygame.display.update()
-        pygame.time.delay(500)
+        pygame.time.delay(2000)
+        pygame.time.set_timer(pygame.USEREVENT, 1000)
 
     def render(self):
         self.screen.fill(WHITE)
         self.grid.draw(self.screen)
         pygame.display.update()
 
-    # TODO:
     def event_handler(self):
+        rules = Rules()
         running = True
 
         while running:
             if self.letter_count < 0:
                 self.letter_count = 0
+            elif self.letter_count > 4:
+                self.letter_count = 4
 
             for event in pygame.event.get():
                 # Player closes window
                 if event.type == pygame.QUIT:
-                    print("Player chose to quit")
+                    # print("Player chose to quit")
                     pygame.quit()
                 if event.type == pygame.KEYDOWN:
-                    print(pygame.key.name(event.key) + " " + str(self.letter_count)) # Test
-
-                    # if self.letter_count > 5:
-                    #         self.error_message("1 Please input a 5-letter word")
-                    #         pass #?
+                    # print(pygame.key.name(event.key) + " " + str(self.letter_count)) # Test
 
                     if event.key == pygame.K_BACKSPACE:
                         self.grid.backspace()
                         self.letter_count -= 1
-                        # self.render()
 
                     elif pygame.key.name(event.key) in LETTERS:
-                        print("In alphabet") # Test
-                        self.letter_count += 1
-                        if self.letter_count > 5: # Account for backspace
-                            self.error_message("Out of space")
+                        # print("In alphabet") # Test
 
                         input_letter = pygame.key.name(event.key)
                         self.grid.enter_letter(input_letter)
-                        # self.render()
+                        self.letter_count += 1
 
                     elif pygame.key.name(event.key) not in LETTERS and event.key != pygame.K_RETURN:
-                        self.error_message("Please input a letter")
-                        print("Not in alphabet") # Test
+                        self.display_message("Please input a letter", False)
+                        # print("Not in alphabet") # Test
 
                     elif event.key == pygame.K_RETURN:
-                        if self.letter_count < 5:
-                            self.error_message("2 Please input a 5-letter word")
-                            # self.grid.backspace()
+                        # if self.letter_count < 4:
+                        if self.grid.current_col < 5:
+                            self.display_message("Please input a 5-letter word", False)
 
                         self.letter_count = 0
                         guessed_word = self.grid.get_word().lower()
                         # Check if word is correct
-                        if self.check_real_word(guessed_word):
-                            # Do the colour thing
-                            print("valid word") # Test
-                            # self.render()
-                        else:
-                            self.error_message("Word not in directory")
-                            print("Word not in directory") # Test
+                        if self.check_real_word(guessed_word) and self.grid.current_col == 5:
+                            # print("valid word") # Test
+                            results = rules.check_colours(guessed_word, self.final_word)
+                            # print(results)
+                            self.grid.enter_word(results)
+                            self.guess += 1
+                            self.render() # Necessary placement
+
+                        if not self.check_real_word(guessed_word) and self.grid.current_col == 5:
+                            self.display_message("Word not in directory", False)
+                            # print("Word not in directory") # Test
                             pass
 
-                        if self.final_word == guessed_word:
-                            print("YAY") # Test
-                            # self.render()
-                    # else:
-                    #     input_letter = pygame.key.name(event.key)
-                    #     self.grid.enter_letter(input_letter)
+                        if guessed_word == self.final_word:
+                            # print("YAY") # Test
+                            pygame.event.set_blocked([pygame.KEYDOWN, pygame.KEYUP])
+                            self.display_message("Congrats!", False)
+
+                        if self.guess == 6 and guessed_word != self.final_word:
+                            print("END") # Test
+                            self.display_message(None, True)
 
             # No more guesses left
             if self.guess > 5:
                 self.render()
 
-            self.render() # TODO: throws traceback error
+            self.render() # TODO: throws traceback error when ctrl+c
 
         pygame.quit()
